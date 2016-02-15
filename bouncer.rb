868bb55a3acb34da37b018948b9fe5ca25bd07f3
Skylar King <sking@crazyawesome.io>
2016-02-15 14:30:44 -0600

    Bouncer works, need to remove 'Nothing' from ages >17.

--- bouncer.rb
+++ bouncer.rb
@@ -1,18 +1,52 @@
 class Bouncer
-
-  def initializer
-  end
+  attr_reader :activities; :age
 
   def start
     puts "====RUNNING BOUNCER===="
-    puts "What is your age?"
-    @age = gets.chomp
-    @age = @age.to_i
-    check_age
+    @activities = [
+      {min_age: 17,
+        activity: "nothing"},
+      {min_age: 18,
+        activity: "vote"},
+      {min_age: 18,
+        activity: "smoke"},
+      {min_age: 21,
+        activity: "drink"},
+      {min_age: 25,
+        activity: "rent a car"}
+    ]
+    age_input
   end
 
-  def check_age
-    
+  def age_input
+    puts "What is your age?"
+    @age = gets.chomp
+    if @age.to_i.to_s == @age
+      @age = @age.to_i
+      age_activities
+    else
+      puts "Please enter an integer"
+      age_input
+    end
+  end
+
+  def age_activities
+    activities
+      .select {|activity| activity[:min_age] <= @age}
+      .map {|activity| activity[:activity]}
+      .each {|activity| puts "You can do #{activity}"}
+    play_again
+  end
+
+  def play_again
+    puts "Would you like to play again? y/n"
+    again = gets.chomp
+    if again == n
+      puts "Bouncer Ended."
+    else
+      puts "Restarting Bouncer..."
+      age_input
+    end
   end
 end
 
